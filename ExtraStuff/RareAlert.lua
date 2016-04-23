89c80480aa41db93774e833739f4af38b503b4c1
aeger <almty1@gmail.com>
2016-04-23 15:26:10 -0700

    Rewriting

--- ExtraStuff/RareAlert.lua
+++ ExtraStuff/RareAlert.lua
@@ -1,18 +1,18 @@
-local name, aStuff = ...
+  local _, aStuff = ...
 
-local function RareAlert()
+	local function RareAlert()
 -- Based completely off Rare Alert by Haleth/Freethinker
 	if not StuffDB["Ralert"] then return end
 
 	local blacklist = {
-	[971] = true, -- Alliance garrison
-	[976] = true, -- Horde garrison
+		[971] = true, -- Alliance garrison
+		[976] = true, -- Horde garrison
 	}
 	
 	if blacklist[GetCurrentMapAreaID()] then return end
 
-	PlaySoundFile("Sound\\Interface\\RaidWarning.ogg")
-	RaidNotice_AddMessage(RaidWarningFrame, "OMFG Rare Get It!", ChatTypeInfo["RAID_WARNING"])
-end
+		PlaySoundFile("Sound\\Interface\\RaidWarning.ogg")
+		RaidNotice_AddMessage(RaidWarningFrame, "OMFG Rare Get It!", ChatTypeInfo["RAID_WARNING"])
+	end
 
-aStuff.RegisterEvent("VIGNETTE_ADDED", RareAlert)
+	aStuff.RegisterEvent("VIGNETTE_ADDED", RareAlert)
--- Stuff.lua
+++ Stuff.lua
@@ -1,44 +1,60 @@
-local name, aStuff = ...
-local CharName = UnitName("player")
+  local _, aStuff = ...
+  local CharName = UnitName("player")
 
 ---------------------------------------------------------------------------
 --							Stuff       								 --
 ---------------------------------------------------------------------------
-
---  Create globals
-	StuffDB = StuffDB or {}
-	StuffCDB = StuffCDB or {}
+		
+	local function aStuffGreeting()
+		if not StuffDB.Greeting then return end
+			DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00Welcome back|r|cFFFF0000 "..CharName.."", 1.0, 1.0, 1.0, nil, true);
+		end
 	
-	local function RunOnce()
 	
+	local function aStuffDBLoad()
 		StuffDB = {
+      DisableChatFade = true,
+      Greeting = true,
       Ralert = true,
-      DisableChatFade = true
-   };
-	
+      Version = 1
+    };
+  	StuffCDB = {
+    	TEST = 1,
+    	TEST2 = 2
+    };
+  end
+  
+  local function aStuffDBupgrade()
+		StuffDB = {
+      DisableChatFade = true,
+      Greeting = true,
+      Ralert = true,
+      Version = 1
+    };
 	end
 	
-	local function StartUp()
+	local function aStuffLoaded()
    if (StuffDB == nil) then
-      RunOnce();
+      aStuffDBLoad();
    end
-   DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00Welcome back|r|cFFFF0000 "..CharName.."|r|cFF00FF00 type |r/aui |cFF00FF00for aegerUI options", 1.0, 1.0, 1.0, nil, true);
-	end
-	
-	aStuff.RegisterEvent("ADDON_LOADED", StartUp)
-
---	Version
-	--LeaPlusLC["AddonVer"] = "6.2.38"
+   if StuffDB.Version ~= 2 then
+   	aStuffDBupgrade()
+   end
+   aStuffGreeting()
+   aStuff:UnregisterEvent("PLAYER_LOGIN")
+  end
+  	
+	aStuff.RegisterEvent("PLAYER_LOGIN", aStuffLoaded)
 
 --  Reload  -------------------------------------------------------------------
-SLASH_RELOADUI1 = "/rl"
-SlashCmdList.RELOADUI = ReloadUI
+  SLASH_RELOADUI1 = "/rl"
+  SlashCmdList.RELOADUI = ReloadUI
 
 --Stacked
-_G.SLASH_FSTACK1 = '/fs'
-SlashCmdList.FSTACK = function(msg) SlashCmdList.FRAMESTACK(msg) end
+  _G.SLASH_FSTACK1 = '/fs'
+  SlashCmdList.FSTACK = function(msg) SlashCmdList.FRAMESTACK(msg) end
 
 -- gm 
-_G.SLASH_HELP1 = '/gm'
-_G.SLASH_HELP2 = '/ticket'
-SlashCmdList.HELP = ToggleHelpFrame
+  _G.SLASH_HELP1 = '/gm'
+  _G.SLASH_HELP2 = '/ticket'
+  SlashCmdList.HELP = ToggleHelpFrame
